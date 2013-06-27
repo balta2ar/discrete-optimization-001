2517ed211e0b063bee8c10f328b19c9a5a91f5a6
Yuri Bochkarev <baltazar.bz@gmail.com>
2013-06-27 17:56:35 +0400

    Add 0screenname

---
+++ 0screenname/solver.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def solveIt():
+    # return a string that is the screen name you would like
+    return 'balta2ar'
+
+if __name__ == '__main__':
+    print 'This script submits the screen name: ' + solveIt() + '\n'
+
