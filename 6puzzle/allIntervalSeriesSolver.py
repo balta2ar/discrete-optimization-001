c9f75d960865a3b9333028612256224cc3e4b2ee
Yuri Bochkarev <baltazar.bz@gmail.com>
2013-06-30 17:57:31 +0400

    Remove trailing spaces

--- 6puzzle/allIntervalSeriesSolver.py
+++ 6puzzle/allIntervalSeriesSolver.py
@@ -3,22 +3,22 @@
 
 def solveIt(n):
     # Modify this code to run your puzzle solving algorithm
-    
+
     # define the domains of all the variables (0..n-1)
     domains = [range(0,n)]*n
-    
+
     # start a trivial depth first search for a solution
     sol = tryall([],domains)
-    
+
     # prepare the solution in the specified output format
     # if no solution is found, put 0s
     outputData = str(n) + '\n'
     if sol == None:
         print 'no solution found.'
         outputData += ' '.join(map(str, [0]*n))+'\n'
-    else: 
+    else:
         outputData += ' '.join(map(str, sol))+'\n'
-    
+
     return outputData
 
 
@@ -31,7 +31,7 @@ def tryall(assignment, domains):
             return assignment
         else:
             return None
-    
+
     # recursive-case: try each value in the next domain
     # if we find a solution return it. otherwise, try the next value
     else:
@@ -44,15 +44,15 @@ def tryall(assignment, domains):
 # checks if an assignment is feasible
 def checkIt(sol):
     n = len(sol)
-    
+
     items = set(sol)
     if len(items) != n:
         return False
-    
+
     deltas = set([abs(sol[i]-sol[i+1]) for i in range(0,n-1)])
     if len(deltas) != n-1:
         return False
-    
+
     return True
 
 
--- 6puzzle/magicSquareSolver.py
+++ 6puzzle/magicSquareSolver.py
@@ -3,17 +3,17 @@
 
 def solveIt(n):
     # Modify this code to run your puzzle solving algorithm
-    
+
     # to be consistent with other example code, we will
     # represent the decision variables as an array (not a matrix)
     # there is no need to do this in your solver
-    
+
     # define the domains of all the variables (1..n*n)
     domains = [range(1,n*n+1)]*(n*n)
-    
+
     # start a trivial depth first search for a solution
     sol = tryall([],domains)
-    
+
     # prepare the solution in the specified output format
     # if no solution is found, put 0s
     outputData = str(n) + '\n'
@@ -21,10 +21,10 @@ def solveIt(n):
         print 'no solution found.'
         for i in range(0,n):
             outputData += ' '.join(map(str, [0]*n))+'\n'
-    else: 
+    else:
         for i in range(0,n):
             outputData += ' '.join(map(str, sol[i*n:(i+1)*n]))+'\n'
-    
+
     return outputData
 
 
@@ -37,7 +37,7 @@ def tryall(assignment, domains):
             return assignment
         else:
             return None
-    
+
     # recursive-case: try each value in the next domain
     # if we find a solution return it. otherwise, try the next value
     else:
@@ -54,15 +54,15 @@ import math
 def checkIt(sol):
     n = int(math.sqrt(len(sol)))
     m = n*(n*n+1)/2
-    
+
     #for i in range(0,n):
     #    print sol[i*n:(i+1)*n]
-    
+
     items = set(sol)
     if len(items) != len(sol):
-        #print len(items),len(sol) 
+        #print len(items),len(sol)
         return False
-    
+
     for i in range(0,n):
         #print 'row',i,sol[i*n:(i+1)*n]
         if sum(sol[i*n:(i+1)*n]) != m:
@@ -72,15 +72,15 @@ def checkIt(sol):
             return False
         if i < n-1:
             if sol[i*n+i] > sol[(i+1)*n+(i+1)]:
-                return False 
-    
+                return False
+
     #print 'diag 1',i,[sol[i*n+i] for i in range(0,n)]
     if sum([sol[i*n+i] for i in range(0,n)]) != m:
         return False
     #print 'diag 2',i,[sol[i*n+(n-i-1)] for i in range(0,n)]
     if sum([sol[i*n+(n-i-1)] for i in range(0,n)]) != m:
         return False
-    
+
     return True
 
 
@@ -94,7 +94,7 @@ if __name__ == "__main__":
             print sys.argv[1].strip(), 'is not an integer'
         print 'Solving Size:', n
         print(solveIt(n))
-    
+
     else:
         print('This test requires an instance size.  Please select the size of problem to solve. (i.e. python magicSquareSolver.py 3)')
 
